af3f725a1ef6049ec1fe18e11bf8cd6c7a3920b7
SavrunE <igor1996igor@inbox.ru>
2020-09-25 10:49:51 +0300

    Start Character

---
+++ HyperCasual/Assets/1EndFiles/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public bool IsGrounded = true;
+    private float jumpPower = 200f;
+    Rigidbody2D body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(Vector2.up * jumpPower);
+        }
+
+    }
+}
